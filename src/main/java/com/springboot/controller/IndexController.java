e41206f2e0a1e535b198c86025d83ddeecd90ebd
githubzwei <mayzwei@gmail.com>
2026-06-01 11:09:59 +0630

    changes 1.6.2026

--- src/main/java/com/springboot/controller/IndexController.java
+++ src/main/java/com/springboot/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.springboot.controller;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -14,15 +16,16 @@ public class IndexController {
 	@GetMapping("/index")
 	public String showIndex(Model m) {
 		m.addAttribute("message", "Springboot");
+		m.addAttribute("today",LocalDate.now());
+		m.addAttribute("today_date", new Date());
 		return "index";
 	}
 	@GetMapping("/users_list")
 	public String showUsers(Model m) {
 		List<String> users = new ArrayList<String>();
-		users.add("Su Su");
-		users.add("Mg Mg");
-		users.add("Ag Ag");
-		
+		/*
+		 * users.add("Su Su"); users.add("Mg Mg"); users.add("Ag Ag");
+		 */
 		m.addAttribute("users_list", users);
 		return "user_list";
 		
